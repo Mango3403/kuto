96e4c60169afef3e2f9f7c4134a9592dcd638587
DESKTOP-KRI1CJB\wzdsr <renhaoming@shinetechchina.com>
2017-10-12 21:00:57 +0800

    添加路由模块，添加表单页面

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import Custom from './components/Custom';
 const Form = () => (
     <div>
         <h1>填写信息</h1>
-        <a href="/">返回</a>        
+        <a href="/custom">返回</a>        
     </div>
 );
 
 const App = () => (
     <Router>
         <div>
-            <Route exact path='/' component={Custom}></Route>
+            <Route exact path='/custom' component={Custom}></Route>
             <Route path='/form' component={Form} />
         </div>
     </Router>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import App from './App';
-import './style.css';
-import Custom from './components/Custom';
+import App from './App';
+// import './style.css';
+// import Custom from './components/Custom';
 
 const MOUNT_NODE = document.getElementById('root');
 
-render(<Custom />, MOUNT_NODE);
+render(<App />, MOUNT_NODE);
