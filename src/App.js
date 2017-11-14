979e41d10cf89bff2f4e2b24712e6fa6ac9b92d7
DESKTOP-KRI1CJB\wzdsr <renhaoming@shinetechchina.com>
2017-11-14 22:57:11 +0800

    更新生成二维码方式

--- src/App.js
+++ src/App.js
@@ -8,15 +8,13 @@ import './App.css';
 import Custom from './components/Custom';
 import CustomForm from './components/CustomForm';
 import CustomHelp from './components/CustomHelp';
-import CustomQRCode from './components/CustomQRCode';
 
 const App = () => (
     <Router basename="/custom">
         <div>
-            <Route exact path="/main" component={Custom} />
+            <Route exact path="/" component={Custom} />
             <Route exact path="/form" component={CustomForm} />
             <Route exact path="/help" component={CustomHelp} />
-            <Route exact path="/" component={CustomQRCode} />
         </div>
     </Router>
 );
