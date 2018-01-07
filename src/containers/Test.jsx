2fcc782f5262a548f6e11083cc4109a2abf60f39
DESKTOP-KRI1CJB\wzdsr <renhaoming@shinetechchina.com>
2018-01-07 22:36:27 +0800

    添加 Fabric 相关模块，更改一些 eslint 规则

---
+++ src/containers/Test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import FabricCanvas from '../components/Fabric/FabricCanvas';
+
+const Test = () => (
+  <FabricCanvas />
+);
+
+export default Test;
--- src/route/routes.jsx
+++ src/route/routes.jsx
@@ -7,6 +7,7 @@ import '../static/css/App.css';
 import Custom from '../containers/Custom';
 import CustomForm from '../containers/CustomForm';
 import CustomHelp from '../containers/CustomHelp';
+import Test from '../containers/Test';
 
 const CustomRouter = () => (
   <Router basename="/custom">
@@ -14,6 +15,7 @@ const CustomRouter = () => (
       <Route path="/" exact component={Custom} />
       <Route path="/form" component={CustomForm} />
       <Route path="/help" component={CustomHelp} />
+      <Route path="/test" component={Test} />
       {/* <Redirect to="/" /> */}
     </div>
   </Router>
