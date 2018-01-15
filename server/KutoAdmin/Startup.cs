7e048f5c3479555f183f0baa724b7bf71f0ca146
DESKTOP-KRI1CJB\wzdsr <renhaoming@shinetechchina.com>
2018-01-15 10:17:14 +0800

    添加 server

---
+++ server/KutoAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KutoAdmin.Startup))]
+namespace KutoAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
